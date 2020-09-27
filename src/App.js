2ad7f65e1e72ae11a910c932205b155b4be46616
arsal iftikhar <arsaliftikhar2@gmail.com>
2020-09-27 14:18:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+
+
+      <h1>Welcome to first Component</h1>
+    
+    
+    
+      )
+  }
+}
+
+export default App;
